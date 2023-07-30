1b869844c71585259ae9a68e0d879c67aef6672b
Andrew Valentovich <andrewvalentovich14@gmail.com>
2023-07-30 23:04:03 +0300

    fix: The same bug but other element

--- resources/views/project/city.blade.php
+++ resources/views/project/city.blade.php
@@ -1651,7 +1651,7 @@
                                                                                    ${{ $object->price }}
                                                                                </div>
                                                                                <div class="kompleks__layout-price-meter" bis_skin_checked="1">
-                                                                                   ${{ intval($object->price/($object->size  ?: 1)) }} / кв.м
+                                                                                   ${{ intval((int) $object->price / ((int) $object->size  ?: 1)) }} / кв.м
                                                                                </div>
                                                                                <div class="kompleks__layout-square" bis_skin_checked="1">
                                                                                    {{ $object->size }}  <span>|</span>  {{ $object->apartment_layout }}
