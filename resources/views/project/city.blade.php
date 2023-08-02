6cbcb544eeca736c04310cbd0cd00e9f93de64d2
Artem Zimin <sasha.lopa188@gmail.com>
2023-08-02 18:45:17 +0300

    fix: gallery slider width & collage header & cheme popup photo

--- resources/views/project/city.blade.php
+++ resources/views/project/city.blade.php
@@ -2411,13 +2411,15 @@
 
     <style>
         .object__photo {
-            position: fixed;
             display: none;
-            left: 0;
+            width: 100%;
+            height: 100%;
+            position: fixed;
             top: 0;
-            bottom: 0;
-            right: 0;
-            z-index: 200;
+            left: 0;
+            overflow-y: auto;
+            overflow-x: hidden;
+            z-index: 165;
         }
         .object__photo.active {
             display: block;
@@ -2434,21 +2436,17 @@
             z-index: 199;
         }
         .object__photo-popup {
-            padding: 0 50px;
-            margin: 0 auto;
-            max-width: 1348px;
             display: flex;
-            justift-content: center;
+            justify-content: center;
             align-items: center;
-            height: 100%;
-            position: relative;
-            z-index: 200;
+            padding: 30px 53px;
+            min-height: 100%;
         }
         .object__photo-popup-block {
             width: 100%;
             position: relative;
             display: flex;
-            justift-content: center;
+            justify-content: center;
             align-items: center;
             z-index: 201;
             opacity: 1;
@@ -2459,6 +2457,7 @@
             margin: 0 auto;
             z-index: 202;
             opacity: 1;
+            width: 100%;
         }
         .object__photo-popup-close {
             position: absolute;
@@ -2470,6 +2469,13 @@
             cursor: pointer;
             color: #fff;
         }
+        @media (max-width: 1364px) {
+            .object__photo-popup-close {
+                right: 15px;
+            }
+        }
+
+
         .object__photo-popup-close svg{
             z-index: 204;
         }
@@ -2480,16 +2486,7 @@
                 <img src="" alt="Картинка">
             </div>
             <div class="object__photo-popup-close">
-                <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" version="1.1"
-                     style="shape-rendering:geometricPrecision;stroke:#fff; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
-                     viewBox="0 0 0.37 0.37"
-                     xmlns:xlink="http://www.w3.org/1999/xlink">
-                     <g id="Слой_x0020_1">
-                         <metadata id="CorelCorpID_0Corel-Layer"/>
-                         <line class="fil0 str0" x1="0.02" y1="0.36" x2="0.36" y2="0.02"/>
-                         <line class="fil0 str0" x1="0.36" y1="0.36" x2="0.02" y2="0.02"/>
-                     </g>
-                </svg>
+            <svg viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>close [#1511]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-419.000000, -240.000000)" fill="#fff"> <g id="icons" transform="translate(56.000000, 160.000000)"> <polygon id="close-[#1511]" points="375.0183 90 384 98.554 382.48065 100 373.5 91.446 364.5183 100 363 98.554 371.98065 90 363 81.446 364.5183 80 373.5 88.554 382.48065 80 384 81.446"> </polygon> </g> </g> </g> </g></svg>
             </div>
         </div>
     </section>
