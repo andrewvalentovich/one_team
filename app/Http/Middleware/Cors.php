2a412fce2f64482a9afcea4d9ed782cff675e946
Andrew Valentovich <andrewvalentovich14@gmail.com>
2023-08-02 19:01:18 +0300

    feat: update Cors middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,8 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', "*")
             ->header('Access-Control-Allow-Methods', "PUT,POST,DELETE,GET,OPTIONS")
-            ->header('Access-Control-Allow-Headers', "Accept,Authorization,Content-Type");
+            ->header('Access-Control-Allow-Headers', "Accept,Authorization,Content-Type")
+            ->header('Content-Type', "application/json")
+            ->header('Content-Type', "application/x-www-form-urlencoded");
     }
 }
