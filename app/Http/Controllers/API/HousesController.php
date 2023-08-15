9b09d80dbf7e984ee86648296577b1f91577d0dd
Andrew Valentovich <andrewvalentovich14@gmail.com>
2023-08-15 15:55:56 +0300

    feat: Modify API for houses

--- app/Http/Controllers/API/HousesController.php
+++ app/Http/Controllers/API/HousesController.php
@@ -6,51 +6,98 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Filters\HousesFilter;
 use App\Http\Requests\House\FilterRequest;
+use App\Models\ExchangeRate;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HousesController extends Controller
 {
+    private $exchanges = [];
+
+    public function __construct()
+    {
+        // Получаем массив с кодом валюты и коэффициентом
+        $this->exchanges = $this->getExchanges();
+    }
+
     public function getByCoordinatesWithFilter(FilterRequest $request)
     {
         $data = $request->validated();
+
+        // Фильтр элементов
         $filter = app()->make(HousesFilter::class, ['queryParams' => $data]);
-        $houses = Product::filter($filter)->with('photo')->with(['favorite' => function ($query) use ($data) {
+        $houses = Product::filter($filter)->with('gostinnie')->with('vanie')->with('spalni')->with('do_more')->with('type_vid')->with('photo')->with(['favorite' => function ($query) use ($data) {
             $query->where('user_id', $data['user_id']);
-        }])->paginate(10);
-//            ->transform(function ($row) {
-//                return [
-//                    'id' => (int) $row->id,
-//                    'country_id' => (int) $row->country_id,
-//                    'city_id' => (int) $row->city_id,
-//                    'sale_or_rent' => (string) $row->sale_or_rent,
-//                    'name' => (string) $row->name,
-//                    'address' => (string) $row->address,
-//                    'size' => (string) $row->size,
-//                    'size_home' => (string) $row->size_home,
-//                    'price' => (int) $row->price,
-//                    'description' => (string) $row->description,
-//                    'description_en' => (string) $row->description_en,
-//                    'description_tr' => (string) $row->description_tr,
-//                    'lat' => (string) $row->lat,
-//                    'long' => (string) $row->long,
-//                    'citizenship' => (string) $row->citizenship,
-//                    'status' => (string) $row->status,
-//                    'disposition' => (string) $row->disposition,
-//                    'disposition_en' => (string) $row->disposition_en,
-//                    'disposition_tr' => (string) $row->disposition_tr,
-//                    'parking' => (string) $row->parking,
-//                    'vnj' => (string) $row->vnj,
-//                    'commissions' => (string) $row->commissions,
-//                    'cryptocurrency' => (string) $row->cryptocurrency,
-//                    'owner' => (string) $row->owner,
-//                    'grajdandstvo' => (string) $row->grajdandstvo,
-//                    'complex_or_not' => (string) $row->complex_or_not,
-//                    'objects' => $row->objects,
-//                ];
-//            })
-//        ->toArray();
+        }])->transform(
+            
+        )->paginate(10);
+
+        // Преобразование цены для записей и поля объекты
+//        foreach ($houses as $house) {
+//            $price = $house['price'];
+//            $objects = [];
+//
+//            // Формируем новое поле price_size
+//            $house['price_size'] = $this->getPriceSize($house['price'], (int) $house['size']);
+//
+//            // Кладём массив с валютой
+//            $house['price'] = $this->getCurrencyPrice($price);
+//            unset($price);
+//
+//
+//            // Если у записи есть объекты
+//            if(isset($house->objects)) {
+//                foreach (json_decode($house->objects) as $object) {
+//                    // Формируем новое поле price_size
+//                    $object->price_size = $this->getPriceSize((int) $object->price, (int) $object->size);
+//
+//                    // Меняем цену
+//                    $price = $object->price;
+//                    $object->price = $this->getCurrencyPrice($price);
+//                    unset($price);
+//
+//                    // Присваиваем объект временной переменой
+//                    $objects[] = $object;
+//                }
+//                $house['objects'] = json_encode($objects, JSON_UNESCAPED_UNICODE);
+//                unset($objects);
+//            }
+//        }
 
         return response()->json($houses);
+    }
+
+    private function getExchanges(): array
+    {
+        // Получаем валюту
+        $exchange_rates = ExchangeRate::where('direct_val', 'EUR')->get();
+        $exchanges = [];
+
+        // Преобразуем в массив вида - ["EUR" => 2.24]
+        foreach ($exchange_rates as $exchange_rate) {
+            $exchanges[$exchange_rate->relative_val] = $exchange_rate->sell_val;
+        }
+
+        return $exchanges;
+    }
+
+    private function getCurrencyPrice(int $price): array
+    {
+        return [
+            "EUR" => number_format($price, 0, '.', ' ')." €",
+            "USD" => number_format($price * $this->exchanges['USD'], 0, '.', ' ')." $",
+            "RUB" => number_format($price * $this->exchanges['RUB'], 0, '.', ' ')." ₽",
+            "TRY" => number_format($price * $this->exchanges['TRY'], 0, '.', ' ')." ₤",
+        ];
+    }
+
+    private function getPriceSize(int $price, int $size = 0): array
+    {
+        return [
+            "EUR" => number_format(ceil($price / (($size) < 1 ? 1 : $size)), 0, '.', ' ')." € / кв.м.",
+            "USD" => number_format(ceil($price * $this->exchanges['USD'] / (($size) < 1 ? 1 : $size)), 0, '.', ' ')." $ / кв.м.",
+            "RUB" => number_format(ceil($price * $this->exchanges['RUB'] / (($size) < 1 ? 1 : $size)), 0, '.', ' ')." ₽ / кв.м.",
+            "TRY" => number_format(ceil($price * $this->exchanges['TRY'] / (($size) < 1 ? 1 : $size)), 0, '.', ' ')." ₤ / кв.м.",
+        ];
     }
 }
