4073aa728daf8a00e1c68dc708e73c78d35c0c36
Zema188 <artem.zimin02@gmail.com>
2023-09-05 19:36:30 +0300

    fix: ballon toggle active/open

--- resources/views/project/houses.blade.php
+++ resources/views/project/houses.blade.php
@@ -2571,12 +2571,16 @@ mapCountry.setBounds([[minLat, minLon], [maxLat, maxLon]], {
                 placemark.events.add('mouseenter', function (e) {
                     if (userAgent.match(/(android|iphone|ipad|ipod|blackberry|windows phone)/)) return
                     placemark.balloon.open(); // Открываем балун при наведении мыши
-                    console.log(placemark)
                     setTimeout(function () {
                         var balloonContentElement = document.querySelector('.balloon-city');
                         const id = balloonContentElement.getAttribute('id')
                         const marks = document.querySelectorAll(`.placemark`);
                         const mark = document.querySelector(`[mark-id="${id}"]`);
+                        if(mark.classList.contains('active')) {
+                            mark.classList.remove('active')
+                            placemark.balloon.close();
+                            return
+                        }
                         marks.forEach(mark => {
                             mark.classList.remove('active')
                         });
