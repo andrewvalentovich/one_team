e1e6fa12b5f14700efbc8773f7620fd4b08ce2de
Zema188 <artem.zimin02@gmail.com>
2023-10-04 23:40:54 +0300

    fix: map height

--- public/project/js/app.js
+++ public/project/js/app.js
@@ -1180,4 +1180,11 @@ if(document.querySelectorAll('#map_city').length) {
 
 }
 
+
+const appHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight()
  
