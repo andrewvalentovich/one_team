b567d95dfb43dc8f24a1139496579762a01ed2cc
zema188 <artem.zimin02@gmail.com>
2023-10-05 22:31:28 +0300

    fix: delete from url param object_id

--- public/project/js/app.js
+++ public/project/js/app.js
@@ -1069,6 +1069,7 @@ if(document.querySelectorAll('.place-w').length) {
             const target = e.target
             if(target.classList.contains('place-w')) {
                 placeBlock.classList.remove('active')
+                url.searchParams.delete('object_id');
             }
             if(target.classList.contains('_country')) {
                 const placeTopImg = document.querySelector('.place__top-img').querySelector('img')
@@ -1077,7 +1078,6 @@ if(document.querySelectorAll('.place-w').length) {
                 placeTopImg.setAttribute('src', '')
                 
             }
-            url.searchParams.delete('object_id');
             // Получение обновленного URL
             var updatedUrl = url.toString();
             // Обновление URL в адресной строке
