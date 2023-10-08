4582f8e041d5008730b6657ca7594ce2faa4bb7c
Andrew Valentovich <andrewvalentovich14@gmail.com>
2023-10-08 18:11:30 +0300

    feat: Add console command to convert images

---
+++ app/Console/Commands/Photos/ConvertExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands\Photos;
+
+use App\Models\PhotoTable;
+use Exception;
+use Illuminate\Console\Command;
+use Intervention\Image\Facades\Image;
+
+class ConvertExtension extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:convert-photo-extension';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Convert photos extension for PhotoTables';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $photos = PhotoTable::all();
+
+        foreach ($photos as $photo) {
+            if (!is_null($photo->photo) && $photo->photo !== "" && $photo->photo !== " ") {
+                dump($photo->photo);
+                try {
+                    $img = Image::make(public_path('uploads/' . $photo->photo));
+
+                    if ($img->extension !== "webp") {
+                        $img->encode('webp', 100);
+                        $img->save(public_path("uploads/" . $img->filename . ".webp"));
+                        $photo->photo = $img->basename;
+                        $photo->save();
+                        dump($photo->photo);
+                    }
+
+                    unset($img);
+                } catch (Exception $exception) {
+                    dump($exception->getMessage());
+                }
+            }
+        }
+    }
+}
---
+++ app/Console/Commands/PreviewPhotos/ConvertExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands\PreviewPhotos;
+
+use App\Models\PhotoTable;
+use Exception;
+use Illuminate\Console\Command;
+use Intervention\Image\Facades\Image;
+
+class ConvertExtension extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:convert-photo-preview-extension';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Convert previews extension for PhotoTables';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $photos = PhotoTable::all();
+
+        foreach ($photos as $photo) {
+            if (!is_null($photo->preview) && $photo->preview !== "" && $photo->preview !== " ") {
+                dump($photo->preview);
+                try {
+                    $img = Image::make(public_path($photo->preview));
+
+                    if ($img->extension !== "webp") {
+                        $img->encode('webp', 100);
+                        $img->save(public_path("uploads/" . $img->filename . ".webp"));
+                        $photo->preview = "uploads/" . $img->basename;
+                        $photo->save();
+                        dump($photo->preview);
+                    }
+
+                    unset($img);
+                } catch (Exception $exception) {
+                    dump($exception->getMessage());
+                }
+            }
+        }
+    }
+}
