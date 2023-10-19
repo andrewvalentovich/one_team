0c34116bf164c6a568c50af076fd26d3625c4ef0
Andrew Valentovich <andrewvalentovich14@gmail.com>
2023-10-19 21:24:24 +0300

    feat: Add redirect & remove unusable features

--- app/Http/Controllers/Front/HousesController.php
+++ app/Http/Controllers/Front/HousesController.php
@@ -14,22 +14,10 @@ use App\Models\ProductCategory;
 
 class HousesController extends Controller
 {
-    public function index(FilterRequest $request)
+    public function index()
     {
-        $data = $request->validated();
-        $exchange_rates = ExchangeRate::where('direct', 'RUB')->get();
-        $exchanges = [];
-//
-        foreach ($exchange_rates as $exchange_rate) {
-            $exchanges[$exchange_rate->relative] = $exchange_rate->value;
-        }
-        $exchanges[$exchange_rates[0]->direct] = 1;
-//
-        $get_product = Product::orderby('price','desc')->paginate(10);
         $country = CountryAndCity::where('id', 17)->first();
         $countries = CountryAndCity::all();
-        $get_city = \App\Models\CountryAndCity::where('parent_id', 17)->get();
-        $count = $get_product->count();
-        return view('project.houses', compact('get_product', 'count', 'country', 'exchanges', 'countries'));
+        return view('project.houses', compact('country', 'countries'));
     }
 }
--- routes/subdomains.php
+++ routes/subdomains.php
@@ -3,43 +3,49 @@
 use App\Models\Landing;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+
 Route::get('/', function ($subdomain) {
-    $currentLanding = Landing::where('subdomain', $subdomain)->get();
+    if ($subdomain === "www") {
+        $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        return redirect(str_replace('www.', '', $url));
+    } else {
+        $currentLanding = Landing::where('subdomain', $subdomain)->get();
 
-    if (!$currentLanding->isEmpty()) {
-        $landing = $currentLanding[0];
+        if (!$currentLanding->isEmpty()) {
+            $landing = $currentLanding[0];
 
-        if ($landing->template->path === "complex") {
-            $filter = \App\Models\Product::with('photo.category')->find($landing['relation_id']);
+            if ($landing->template->path === "complex") {
+                $filter = \App\Models\Product::with('photo.category')->find($landing['relation_id']);
 
-            abort_if(!isset($filter), 404);
+                abort_if(!isset($filter), 404);
 
-            $categories =
-                DB::table('products')->select(['photo_categories.id', 'photo_categories.name'])
-                    ->where('products.id', $filter->id)
-                    ->join('photo_tables', 'photo_tables.parent_id', '=', 'products.id')
-                    ->join('photo_categories', 'photo_categories.id', '=', 'photo_tables.category_id')
-                    ->get();
+                $categories =
+                    DB::table('products')->select(['photo_categories.id', 'photo_categories.name'])
+                        ->where('products.id', $filter->id)
+                        ->join('photo_tables', 'photo_tables.parent_id', '=', 'products.id')
+                        ->join('photo_categories', 'photo_categories.id', '=', 'photo_tables.category_id')
+                        ->get();
 
-            return view("landings/complex", compact('landing', 'filter', 'categories'));
+                return view("landings/complex", compact('landing', 'filter', 'categories'));
+            }
+
+            if ($landing->template->path === "region") {
+                $filter = \App\Models\CountryAndCity::find($landing['relation_id']);
+                abort_if(!isset($filter), 404);
+                $types = \App\Models\Peculiarities::where('type', 'Типы')->get();
+                return view("landings/region", compact('landing', 'filter', 'types'));
+            }
+
+            if ($landing->template->path === "country") {
+                $filter = \App\Models\CountryAndCity::find($landing['relation_id']);
+                abort_if(!isset($filter), 404);
+                $types = \App\Models\Peculiarities::where('type', 'Типы')->get();
+                return view("landings/country", compact('landing', 'filter', 'types'));
+            }
         }
 
-        if ($landing->template->path === "region") {
-            $filter = \App\Models\CountryAndCity::find($landing['relation_id']);
-            abort_if(!isset($filter), 404);
-            $types = \App\Models\Peculiarities::where('type', 'Типы')->get();
-            return view("landings/region", compact('landing', 'filter', 'types'));
-        }
+        abort_if(!isset($landing), 404);
 
-        if ($landing->template->path === "country") {
-            $filter = \App\Models\CountryAndCity::find($landing['relation_id']);
-            abort_if(!isset($filter), 404);
-            $types = \App\Models\Peculiarities::where('type', 'Типы')->get();
-            return view("landings/country", compact('landing', 'filter', 'types'));
-        }
+        return false;
     }
-
-    abort_if(!isset($landing), 404);
-
-    return false;
 });
