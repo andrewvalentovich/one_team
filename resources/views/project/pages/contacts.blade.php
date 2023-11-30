6d54b7d28eb7cc62a67c2a66575e8a60b353e0e8
Zema188 <artem.zimin02@gmail.com>
2023-11-30 13:19:36 +0300

    add: content in contacts page

--- resources/views/project/pages/contacts.blade.php
+++ resources/views/project/pages/contacts.blade.php
@@ -15,8 +15,54 @@
 </style>
 
 @section('content')
-    <?php echo json_decode($get->content )  ?>
-
+    <!-- <?php echo json_decode($get->content )  ?> -->
+    <div class="about">
+        <div class="hello__title title">
+            Контакты
+        </div>
+        <div class="contacts maxContainer">
+            <ul class="contacts__list">
+                <div class="contacts__subtitle">
+                    Контакты:
+                </div>
+                <li>
+                    <a href="tel:+74951067579">+74951067579</a>
+                    <span>
+                        Москва
+                    </span>
+                </li>
+                <li>
+                    <a href="tel:+998712051677">+998712051677</a>
+                    <span>
+                        Узбекистан
+                    </span>
+                </li>
+                <li>
+                    <a href="tel:+77172727498">+77172727498</a>
+                    <span>
+                        Казахстан
+                    </span>
+                </li>
+                <li>
+                    <a href="tel:+447478215412">+447478215412</a>
+                    <span>
+                        Лондон
+                    </span>
+                </li>
+            </ul>
+            <ul class="contacts__list">
+                <div class="contacts__subtitle">
+                    Адрес:
+                </div>
+                <li>
+                    <span>
+                    Анталья, Коньяалты, Торос, улица 815, 1
+                    </span>
+                </li>
+            </ul>
+            <iframe class="contacts__map" src="https://yandex.ru/map-widget/v1/?um=constructor%3A2e01e76fb81817ab7abc5adae305c6ccc877665e78da5ea6ab8abbedc7d5cae0&amp;source=constructor" frameborder="0"></iframe>
+        </div>
+    </div>
 @endsection
 
 
