fca1ab35c79427c4f0ec3a3d0687a161717ad81d
root <root@www.one-team.pro>
2023-11-30 11:36:44 +0000

    fix: Uncommit crm:import-complex

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('products:update-base-price')->daily();
         $schedule->command('layouts:update-base-price')->daily();
         $schedule->command('sitemap:generate')->daily();
-        //$schedule->command('crm:import-complex 8')->everyTenMinutes();
+        $schedule->command('crm:import-complex 8')->everyTenMinutes();
         Log::info(date('m/d/Y h:i:s a', time()) . " Schedule called");
     }
 
