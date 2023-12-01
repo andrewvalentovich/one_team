d72b1143c60ad88075d7e533a84f4dce85b2e51f
Zema188 <artem.zimin02@gmail.com>
2023-12-01 19:20:09 +0300

    add preferredCountries in phone input

--- public/project/js/tel-input.js
+++ public/project/js/tel-input.js
@@ -6,4 +6,5 @@
 const input = document.querySelector("#phone");
 window.intlTelInput(input, {
     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
+    preferredCountries: ['ru', 'tr']
 });
