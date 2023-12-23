b78cd391684299566346138bf3d81d21eafad113
zema188 <artem.zimin02@gmail.com>
2023-12-23 21:05:30 +0300

    minor fix

--- resources/views/project/includes/form-popup.blade.php
+++ resources/views/project/includes/form-popup.blade.php
@@ -1,17 +1,17 @@
 <div class="popup popup-modal main-form-popup" style="z-index: 2005!important;" bis_skin_checked="1">
     <div class="popup__body" bis_skin_checked="1">
         <form class="popup__content default-form" id="city_form">
-            <label class="field email input-wrapper hidden" bis_skin_checked="1">
-                    <span class="text">
-                        {{__('Email')}}
-                    </span>
-                <input type="text" value="" placeholder="{{ __('example@mail.ru') }}" name="email">
-            </label>
             <label class="field name input-wrapper" bis_skin_checked="1">
                     <span class="text">
                         {{__('ФИО')}}
                     </span>
                 <input type="text" value="" placeholder="{{ __('Иванов Алексей Петрович') }}" name="fio">
+            </label>
+            <label class="field email input-wrapper hidden" bis_skin_checked="1">
+                    <span class="text">
+                        {{__('Email')}}
+                    </span>
+                <input type="text" value="" placeholder="{{ __('example@mail.ru') }}" name="email">
             </label>
             <div class="contact__form-phone input-wrapper">
                     <span class="text">
