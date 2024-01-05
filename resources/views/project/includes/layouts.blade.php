c310964d41a392094778639c5598b60509d2bca0
Andrew Valentovich <andrewvalentovich14@gmail.com>
2024-01-05 22:36:33 +0300

    feat: Add gtag

--- resources/views/project/includes/layouts.blade.php
+++ resources/views/project/includes/layouts.blade.php
@@ -36,6 +36,15 @@
         window.locale = '{{ app()->getLocale() }}';
     </script>
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11425094300"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-11425094300');
+</script>
 <body>
 <div class="wrapper">
 
